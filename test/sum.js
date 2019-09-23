471d5562c0002072be61008e897a7a1a9404c6c3
Odhran Daly <odhran.daly25@mail.dcu.ie>
2019-09-23 21:42:34 +0100

    node install

---
+++ test/sum.js
@@ -0,0 +1,23 @@
+/* test/sum.js */
+
+var expect = require('chai').expect;
+
+describe('#', function() {
+
+  context('without arguments', function() {
+    it('should return 0', function() {
+      expect(0).to.equal(0)
+    })
+  })
+
+  context('with number arguments', function() {
+    it('should return sum of arguments', function() {
+      expect(15).to.equal(15)
+    })
+
+    it('should return argument when only one argument is passed', function() {
+      expect(5).to.equal(5)
+    })
+  })
+
+})
